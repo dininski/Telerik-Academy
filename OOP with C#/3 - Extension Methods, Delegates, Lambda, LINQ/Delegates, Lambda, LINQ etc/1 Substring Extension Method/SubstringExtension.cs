441b2c599544034e331c68f3782f32fce3b04ae4
dininski <dininski@gmail.com>
2013-02-27 15:34:55 +0200

    hw3 1st assignment

---
+++ OOP with C#/3 - Extension Methods, Delegates, Lambda, LINQ/Delegates, Lambda, LINQ etc/1 Substring Extension Method/SubstringExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+public static class StringBuilderExtension
+{
+    public static string Substring(this StringBuilder str, int index, int length)
+    {
+        return str.ToString().Substring(index,length);
+    }
+
+    static void Main(string[] args)
+    {
+        StringBuilder someString = new StringBuilder();
+        someString.Append("This is a test string");
+        Console.WriteLine(someString.Substring(2, 4));
+    }
+}
