5f37e8958265a504bacdd906bd86ea2d1139ae96
Vasil Dininski <dininski@gmail.com>
2013-03-12 14:03:17 +0200

    JS and algo
    Finished JS homework 3, started homework 4, created Algo Academy
    directory and solved first problem

---
+++ Algo Academy/1 Dynamic Optimization/1 Tribonacci/Tribonacci.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Tribonacci
+{
+    static long CalculateIt(long[] calculated, int elementToFind, int current)
+    {
+        calculated[current] = (calculated[current - 1] + calculated[current - 2] + calculated[current - 3]);
+        if (elementToFind == current)
+        {
+            return calculated[current];
+        }
+        else
+        {
+            return CalculateIt(calculated, elementToFind, current + 1);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        long[] tribonacci; ;
+        string[] input = Console.ReadLine().Split(' ');
+        int searchedElement = int.Parse(input[3]);
+        if (searchedElement < 3)
+        {
+            tribonacci = new long[3];
+        }
+        else
+        {
+            tribonacci = new long[searchedElement];
+        }
+
+        tribonacci[0] = long.Parse(input[0]);
+        tribonacci[1] = long.Parse(input[1]);
+        tribonacci[2] = long.Parse(input[2]);
+
+        if (searchedElement <= 3)
+        {
+            Console.WriteLine(tribonacci[searchedElement-1]);
+        }
+        else
+        {
+            Console.WriteLine(CalculateIt(tribonacci, searchedElement - 1, 3));
+        }
+    }
+}
---
+++ Algo Academy/1 Dynamic Optimization/2 SuperSum/SuperSum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class SuperSum
+{
+    static int[,] sums;
+    public static int SSum(int K, int N)
+    {
+        if (K == 0)
+        {
+            sums[K, N] = N;
+            return N;
+        }
+        else
+        {
+            for (int i = 1; i < N + 1; i++)
+            {
+                sums[K, N] += sums[K-1, i];
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(' ');
+        int K = int.Parse(input[0]);
+        int N = int.Parse(input[1]);
+        sums = new int[K, N];
+        Console.WriteLine(SSum(K,N));
+    }
+}
