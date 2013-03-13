8117f2e5cfac92290d87dfd8e90c85ea26b4d6b0
Vasil Dininski <dininski@gmail.com>
2013-03-14 01:43:42 +0200

    Finished OOP basics p.2 hw

--- OOP with C#/5 - OOP Principles II/Range Exception/InvalidRangeException.cs
+++ OOP with C#/5 - OOP Principles II/Range Exception/InvalidRangeException.cs
@@ -1,6 +1,22 @@
 ﻿using System;
 
+//Define a class InvalidRangeException<T> that holds information about an error 
+//condition related to invalid range. It should hold error message and a range 
+//definition [start … end].
+//Write a sample application that demonstrates the InvalidRangeException<int> and 
+//InvalidRangeException<DateTime> by entering numbers in the range [1..100] and 
+//dates in the range [1.1.1980 … 31.12.2013].
+
+
+
 public class InvalidRangeException<T> : Exception
 {
+    public T Min { get; private set; }
+    public T Max { get; private set; }
 
+    public InvalidRangeException(string message, T min, T max) : base(message)
+    {
+        this.Min = min;
+        this.Max = max;
+    }
 }
---
+++ OOP with C#/5 - OOP Principles II/Range Exception/TestTheException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class TestTheException
+{
+    public static void TestInt(int someNumber)
+    {
+        
+    }
+
+    static void Main(string[] args)
+    {
+        InvalidRangeException<int> integerException = new InvalidRangeException<int>("Value must be in range 1 - 100", 1, 100);
+        InvalidRangeException<DateTime> dateException = new InvalidRangeException<DateTime>("Date must be between 1.1.1980 and 31.12.2013", 
+            new DateTime(1980,1,1), new DateTime(2013,12,31));
+
+        Console.WriteLine("Please enter 3 values between 1 and 100 (if you don't want errors)");
+        for (int i = 0; i < 3; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (number < integerException.Min || number > integerException.Max)
+            {
+                throw integerException;
+            }
+        }
+
+        Console.WriteLine("Please enter 3 dates between 01.01.1980 and 31.12.2013");
+        for (int i = 0; i < 3; i++)
+        {
+            DateTime date = DateTime.Parse(Console.ReadLine());
+            if (date < dateException.Min || date > dateException.Max)
+            {
+                throw dateException;
+            }
+        }
+    }
+}
