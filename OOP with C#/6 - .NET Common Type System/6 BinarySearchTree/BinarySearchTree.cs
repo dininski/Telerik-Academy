32b6293a3f00f40995141fb53017bd88df5bd50f
Vasil Dininski <dininski@gmail.com>
2013-03-24 18:39:38 +0200

    Started BST problem

---
+++ OOP with C#/6 - .NET Common Type System/6 BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BST<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>, ICloneable where TKey : IComparable<TKey>
+    {
+        private TreeNode root = null;
+
+        private class TreeNode
+        {
+            public TKey Key { get; set; }
+            public TValue Value { get; set; }
+            public TreeNode Previous { get; set; }
+            public TreeNode Next { get; set; }
+            public TreeNode Parent { get; set; }
+
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TreeNode node = new TreeNode();
+            node.Key = key;
+            node.Value = value;
+
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+
+        public object Clone()
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+    }
+}
