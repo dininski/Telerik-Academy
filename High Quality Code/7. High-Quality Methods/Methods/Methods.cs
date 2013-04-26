406603e0b9f02371fc8b211394ff19d5fb34f981
Vasil Dininski <dininski@gmail.com>
2013-04-26 11:35:53 +0200

    Refactoring, finished large portion of hw

--- High Quality Code/7. High-Quality Methods/Methods/Methods.cs
+++ High Quality Code/7. High-Quality Methods/Methods/Methods.cs
@@ -41,7 +41,8 @@
             {
                 throw new ArgumentNullException("Argument must not be null!");
             }
-            else if (elements.Length == 0)
+
+            if (elements.Length == 0)
             {
                 throw new ArgumentException("At least one argument must be provided!");
             }
@@ -58,34 +59,35 @@
             return maxNumber;
         }
 
-        public static void PrintAsNumber(object number, string format)
+        public static void PrintAsPercent(object number)
         {
-            // replace with switch
-            if (format == "f")
-            {
-                Console.WriteLine("{0:f2}", number);
-            }
-
-            if (format == "%")
-            {
-                Console.WriteLine("{0:p0}", number);
-            }
-
-            if (format == "r")
-            {
-                Console.WriteLine("{0,8}", number);
-            }
+            Console.WriteLine("{0:p0}", number);
         }
 
+        public static void PrintWithOffset(object number)
+        {
+            Console.WriteLine("{0,8}", number);
+        }
+
+        public static void PrintFloatWithPrecision(object number)
+        {
+            Console.WriteLine("{0:f2}", number);
+        }
+
+        // TODO
         public static double CalcDistance(double x1, double y1, double x2, double y2,
             out bool isHorizontal, out bool isVertical)
         {
-            // split in separate methods
             isHorizontal = (y1 == y2);
             isVertical = (x1 == x2);
 
             double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
             return distance;
+        }
+
+        public static bool CheckIfInSameDimension(bool dimensionOfFirst, bool dimensionOfSecond)
+        {
+            return dimensionOfFirst == dimensionOfSecond;
         }
 
         public static void Main()
@@ -96,20 +98,23 @@
 
             Console.WriteLine(FindMax(5, -1, 3, 2, 14, 2, 3));
 
-            PrintAsNumber(1.3, "f");
-            PrintAsNumber(0.75, "%");
-            PrintAsNumber(2.30, "r");
+            PrintFloatWithPrecision(1.3);
+            PrintAsPercent(0.75);
+            PrintWithOffset(2.30);
 
-            bool horizontal, vertical;
+            bool horizontal;
+            bool vertical;
             Console.WriteLine(CalcDistance(3, -1, 3, 2.5, out horizontal, out vertical));
             Console.WriteLine("Horizontal? " + horizontal);
             Console.WriteLine("Vertical? " + vertical);
 
             Student peter = new Student() { FirstName = "Peter", LastName = "Ivanov" };
-            peter.OtherInfo = "From Sofia, born at 17.03.1992";
+            peter.ExtraInfo = "From Sofia";
+            peter.DateOfBirth = DateTime.Parse("03/17/1992");
 
             Student stella = new Student() { FirstName = "Stella", LastName = "Markova" };
-            stella.OtherInfo = "From Vidin, gamer, high results, born at 03.11.1993";
+            stella.ExtraInfo = "From Vidin, gamer, high results";
+            stella.DateOfBirth = DateTime.Parse("11/03/1993");
 
             Console.WriteLine("{0} older than {1} -> {2}", peter.FirstName, stella.FirstName, peter.IsOlderThan(stella));
         }
--- High Quality Code/7. High-Quality Methods/Methods/Student.cs
+++ High Quality Code/7. High-Quality Methods/Methods/Student.cs
@@ -8,16 +8,13 @@
 
         public string LastName { get; set; }
 
-        // change to DateOfBirth and add another field for extra info - refactor then
-        public string OtherInfo { get; set; }
+        public string ExtraInfo { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
 
         public bool IsOlderThan(Student other)
         {
-            DateTime firstDate =
-                DateTime.Parse(this.OtherInfo.Substring(this.OtherInfo.Length - 10));
-            DateTime secondDate =
-                DateTime.Parse(other.OtherInfo.Substring(other.OtherInfo.Length - 10));
-            return firstDate > secondDate;
+            return this.DateOfBirth > other.DateOfBirth;
         }
     }
 }
