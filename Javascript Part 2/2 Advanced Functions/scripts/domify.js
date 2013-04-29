793b39b04f946e45930a41e2ef43d91b90223ea1
Vasil Dininski <dininski@gmail.com>
2013-04-29 22:05:00 +0300

    Started DOM module

--- Javascript Part 2/2 Advanced Functions/scripts/domify.js
+++ Javascript Part 2/2 Advanced Functions/scripts/domify.js
@@ -1,12 +1,32 @@
-(function() {
-	'use strict';
+var domify = (function () {
+    var appendChild = function (elementToAppend, parentElement) {
+        var parent = document.querySelector(parentElement);
+        parent.appendChild(elementToAppend);
+    }
 
-	var Domify = function() {
-		this.appendChild = function(child, parent) {
-			var parentToAppendTo = document.querySelector(parent);
-			console.log(parentToAppendTo);
-			parentToAppendTo.appendChild(child);
-		}
-	}
-	
+    var removeChild = function (parentElements, elementToRemove) {
+        var parents = document.querySelectorAll(parentElements);
+
+        // check if the element to remove is a pseudo element
+        if (elementToRemove.indexOf(':') > 0) {
+            var pseudoElement = elementToRemove.substr(elementToRemove.indexOf(':') + 1);
+            switch (pseudoElement) {
+                case 'first':
+                    break;
+                case 'second':
+                    break;
+            }
+        }
+    }
+
+    var addHandler = function (eventElement, eventType, callback) {
+        var elementToAddHandlerTo = document.querySelector(eventElement);
+        elementToAddHandlerTo.addEventListener(eventType, callback);
+    }
+
+    return {
+        appendChild: appendChild,
+        removeChild: removeChild,
+        addHandler: addHandler
+    };
 })();
