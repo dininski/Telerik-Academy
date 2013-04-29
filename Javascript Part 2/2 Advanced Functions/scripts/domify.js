2d7b993f656c224d97ebf2479b3c1378248cfd4a
Vasil Dininski <dininski@gmail.com>
2013-04-29 22:36:48 +0300

    Added most methods
    TODO: finish addToBuffer() method

--- Javascript Part 2/2 Advanced Functions/scripts/domify.js
+++ Javascript Part 2/2 Advanced Functions/scripts/domify.js
@@ -4,29 +4,56 @@ var domify = (function () {
         parent.appendChild(elementToAppend);
     }
 
-    var removeChild = function (parentElements, elementToRemove) {
-        var parents = document.querySelectorAll(parentElements);
+    var removeChild = function (parents, elementToRemove) {
+        var parentElements = document.querySelectorAll(parents);
 
         // check if the element to remove is a pseudo element
         if (elementToRemove.indexOf(':') > 0) {
             var pseudoElement = elementToRemove.substr(elementToRemove.indexOf(':') + 1);
+            var element = elementToRemove.substr(0, elementToRemove.indexOf(':'));
             switch (pseudoElement) {
                 case 'first':
+                    removeFirstChild(parentElements, element);
                     break;
-                case 'second':
+                case 'last':
+                    removeLastChild(parentElements, element);
                     break;
             }
+        } else {
+
         }
     }
 
-    var addHandler = function (eventElement, eventType, callback) {
+    // this method is not exposed to the public
+    var removeFirstChild = function (parentElements, elementToRemove) {
+        for (var i = 0; i < parentElements.length; i++) {
+            var firstElement = parentElements[i].querySelector(elementToRemove);
+            parentElements[i].removeChild(firstElement);
+        }
+    }
+
+    // this method is not exposed to the public
+    var removeLastChild = function (parentElements, elementToRemove) {
+        for (var i = 0; i < parentElements.length; i++) {
+            var allChildren = parentElements[i].querySelectorAll(elementToRemove);
+            var pseudoElementToRemove= allChildren[allChildren.length - 1];
+            parentElements[i].removeChild(pseudoElementToRemove);
+        }
+    }
+
+    var addHandler = function (eventElement, event, callback) {
         var elementToAddHandlerTo = document.querySelector(eventElement);
-        elementToAddHandlerTo.addEventListener(eventType, callback);
+        elementToAddHandlerTo.addEventListener(event, callback);
+    }
+
+    // TODO: to finish buffer
+    var addToBuffer = function() {
     }
 
     return {
         appendChild: appendChild,
         removeChild: removeChild,
-        addHandler: addHandler
+        addHandler: addHandler,
+        addToBuffer: addToBuffer
     };
 })();
