eb4ed6a3587be772ec19bc83f8242a363577546b
Vasil Dininski <dininski@gmail.com>
2013-04-29 21:31:27 +0300

    Finished assertions

---
+++ High Quality Code/9. Defensive Programming and Exceptions/Assertions-Homework/AssertionsHomework.cs
@@ -0,0 +1,114 @@
+﻿namespace AssertionsHomework
+{
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
+
+    public class AssertionsHomework
+    {
+        public static void SelectionSort<T>(T[] arr) where T : IComparable<T>
+        {
+            Debug.Assert(arr != null, "The array cannot be null!");
+
+            for (int index = 0; index < arr.Length - 1; index++)
+            {
+                int minElementIndex = FindMinElementIndex(arr, index, arr.Length - 1);
+                Swap(ref arr[index], ref arr[minElementIndex]);
+            }
+
+            // check if the array has been properly sorted
+            for (int i = 1; i < arr.Length - 1; i++)
+            {
+                Debug.Assert(arr[i - 1].CompareTo(arr[i]) <= 0, "The array is not properly sorted!");
+            }
+        }
+
+        public static void Main()
+        {
+            int[] arr = new int[] { 3, -1, 15, 4, 17, 2, 33, 0 };
+            Console.WriteLine("arr = [{0}]", string.Join(", ", arr));
+            SelectionSort(arr);
+            Console.WriteLine("sorted = [{0}]", string.Join(", ", arr));
+
+            SelectionSort(new int[0]); // Test sorting empty array
+            SelectionSort(new int[1]); // Test sorting single element array
+
+            Console.WriteLine(BinarySearch(arr, -1000));
+            Console.WriteLine(BinarySearch(arr, 0));
+            Console.WriteLine(BinarySearch(arr, 17));
+            Console.WriteLine(BinarySearch(arr, 10));
+            Console.WriteLine(BinarySearch(arr, 1000));
+        }
+
+        public static int BinarySearch<T>(T[] arr, T value) where T : IComparable<T>
+        {
+            return BinarySearch(arr, value, 0, arr.Length - 1);
+        }
+
+        private static int BinarySearch<T>(T[] arr, T value, int startIndex, int endIndex)
+            where T : IComparable<T>
+        {
+            // Check if the array has been sorted
+            for (int i = 1; i < arr.Length - 1; i++)
+            {
+                Debug.Assert(arr[i - 1].CompareTo(arr[i]) <= 0, "The array is not correctly sorted!");
+            }
+
+            while (startIndex <= endIndex)
+            {
+                int midIndex = (startIndex + endIndex) / 2;
+                if (arr[midIndex].Equals(value))
+                {
+                    return midIndex;
+                }
+
+                if (arr[midIndex].CompareTo(value) < 0)
+                {
+                    // Search on the right half
+                    startIndex = midIndex + 1;
+                }
+                else
+                {
+                    // Search on the right half
+                    endIndex = midIndex - 1;
+                }
+            }
+
+            // Searched value not found
+            return -1;
+        }
+
+        private static int FindMinElementIndex<T>(T[] arr, int startIndex, int endIndex)
+            where T : IComparable<T>
+        {
+            // Check if the arguments are valid
+            Debug.Assert(arr != null, "The array cannot be null!");
+            Debug.Assert(startIndex >= 0, "The start index cannot be negative!");
+            Debug.Assert(startIndex < endIndex, "The ending index cannot be before the starting index!");
+            Debug.Assert(endIndex <= arr.Length - 1, "The ending index cannot be bigger than the lenght of the array!");
+
+            int minElementIndex = startIndex;
+            for (int i = startIndex + 1; i <= endIndex; i++)
+            {
+                if (arr[i].CompareTo(arr[minElementIndex]) < 0)
+                {
+                    minElementIndex = i;
+                }
+            }
+
+            // Check if the smallest element was correctly found using lambda expression
+            Debug.Assert(
+                arr[minElementIndex].CompareTo(arr.Skip(startIndex).Take(endIndex - startIndex + 1).Min()) == 0,
+                "The smallest element was incorrectly found");
+
+            return minElementIndex;
+        }
+
+        private static void Swap<T>(ref T x, ref T y)
+        {
+            T oldX = x;
+            x = y;
+            y = oldX;
+        }
+    }
+}
---
+++ High Quality Code/9. Defensive Programming and Exceptions/Exceptions-Homework/CSharpExam.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class CSharpExam : Exam
+{
+    public int Score { get; private set; }
+
+    public CSharpExam(int score)
+    {
+        if (score < 0)
+        {
+            throw new ArgumentException("The score cannot be less than 0!");
+        }
+
+        this.Score = score;
+    }
+
+    public override ExamResult Check()
+    {
+        if (Score < 0 || Score > 100)
+        {
+            throw new ArgumentOutOfRangeException("The score must be positive and smaller or equal to 100");
+        }
+        else
+        {
+            return new ExamResult(this.Score, 0, 100, "Exam results calculated by score.");
+        }
+    }
+}
