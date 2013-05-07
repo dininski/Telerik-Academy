b32224a3a70f364f7bb228e33c731fad3373d8e6
Vasil Dininski <dininski@gmail.com>
2013-05-07 13:07:11 +0300

    Finished todo list

--- Javascript Part 2/4 Javascript Event Model/scripts/todolify.js
+++ Javascript Part 2/4 Javascript Event Model/scripts/todolify.js
@@ -3,6 +3,7 @@ var todolify = (function () {
 
     var todoList = document.querySelector('#todo-list');
     var hiddenItems = new Array();
+    var currentElementId = 0;
 
     var addItem = function (date, todoContent) {
         if (!date) {
@@ -13,9 +14,21 @@ var todolify = (function () {
             throw new Error('The content of the todo item cannot be empty!');
         }
 
-        var newItem = document.createElement('option');
-        newItem.innerText = date + ': ' + todoContent;
-        todoList.appendChild(newItem);
+        var newTodoListItem = document.createElement('div');
+
+        var radioButton = document.createElement('input');
+        radioButton.setAttribute('type', 'radio');
+        radioButton.setAttribute('id', currentElementId);
+        radioButton.setAttribute('name', 'todo list item');
+        newTodoListItem.appendChild(radioButton);
+
+        var labelForRadioButton = document.createElement('label');
+        labelForRadioButton.setAttribute('for', currentElementId);
+        labelForRadioButton.innerHTML = date + ': ' + todoContent;
+        newTodoListItem.appendChild(labelForRadioButton);
+
+        currentElementId++;
+        todoList.appendChild(newTodoListItem);
     }
 
     var removeItem = function () {
@@ -23,21 +36,37 @@ var todolify = (function () {
             throw new Error('No item is selected for removal!');
         }
 
-        todoList.removeChild(todoList[todoList.selectedIndex]);
+        var itemToRemove = getCheckedItem();
+
+        todoList.removeChild(itemToRemove);
     }
 
     var hideItem = function () {
-        var itemToHide = todoList[todoList.selectedIndex];
+
+        var itemToHide = getCheckedItem();
+
         itemToHide.style.visibility = 'hidden';
+        itemToHide.style.position = 'absolute';
+
         hiddenItems.push(itemToHide);
     }
 
     var showItems = function () {
         for (var i = 0; i < hiddenItems.length; i++) {
             hiddenItems[i].style.visibility = 'visible';
+            hiddenItems[i].style.position = 'static';
         }
 
         hiddenItems = new Array();
+    }
+
+    var getCheckedItem = function () {
+        var allItems = todoList.children;
+        for (var i = 0; i < allItems.length; i++) {
+            if (allItems[i].firstChild.checked) {
+                return allItems[i];
+            }
+        }
     }
 
     return {
