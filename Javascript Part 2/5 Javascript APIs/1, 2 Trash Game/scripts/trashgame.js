04ef984609e33a15da141ba200dd8c38b8b997ec
Vasil Dininski <dininski@gmail.com>
2013-05-08 22:50:33 +0300

    creation of trash elements

--- Javascript Part 2/5 Javascript APIs/1, 2 Trash Game/scripts/trashgame.js
+++ Javascript Part 2/5 Javascript APIs/1, 2 Trash Game/scripts/trashgame.js
@@ -1,7 +1,20 @@
-var trashgame = (function () {
-    var numberOfTrash = 10;
+(function () {
+    var numberOfTrash = 15;
+    
+    var trash = document.createElement('div');
+    var trashImage = document.createElement('img');
+    trashImage.setAttribute('src','images/trash.png');
+    trash.appendChild(trashImage.cloneNode(true));
+    trash.setAttribute('draggable', 'true');
+    trash.style.position = 'relative';
+    trash.style.right = Math.random() * document.body.scrollWidth ;
+    trash.style.top = Math.random() * document.body.scrollHeight;
+    trash.style.width = '20px';
 
-    return {
-
+    for (var i = 0; i < numberOfTrash; i++) {
+        var newChild = trash.cloneNode(true);
+        newChild.style.left = Math.random() * window.innerWidth;
+        newChild.style.top = Math.random() * window.innerHeight;
+        document.querySelector('body').appendChild(newChild);
     }
 })();
