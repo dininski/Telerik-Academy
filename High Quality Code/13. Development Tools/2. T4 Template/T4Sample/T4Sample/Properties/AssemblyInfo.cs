84ac61387cf67a0c99106a3e8a0b48d893740f79
anon <lu6627@auoie.com>
2013-05-17 17:02:55 +0300

    Dev tools hw

---
+++ High Quality Code/13. Development Tools/2. T4 Template/T4Sample/T4Sample/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("T4Sample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("T4Sample")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3c6dc5a9-f7da-4a88-9542-8cd3f745d44c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ High Quality Code/13. Development Tools/4. Test app for Log4Net/TestAppLog4Net/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using log4net;
+using log4net.Config;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAppLog4Net
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            BasicConfigurator.Configure();
+            log.Info("app initalizes...");
+            TestClassToIllustrate tc = new TestClassToIllustrate();
+            tc.Start();
+            log.Info("app finished...");
+        }
+    }
+
+    public class TestClassToIllustrate {
+
+        private static readonly ILog log = LogManager.GetLogger(typeof(TestClassToIllustrate));
+
+        public void Start()
+        {
+            log.Info("In another method!");
+        }
+    }
+}
