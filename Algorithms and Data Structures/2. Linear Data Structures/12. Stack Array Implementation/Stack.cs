7c72b2f309643de8c97a475e88ece46eebadf856
dininski <dininski@gmail.com>
2013-05-29 19:53:41 +0300

    Added Stack with array implementation

---
+++ Algorithms and Data Structures/2. Linear Data Structures/12. Stack Array Implementation/Stack.cs
@@ -0,0 +1,61 @@
+﻿// Implement the ADT stack as auto-resizable array. Resize
+// the capacity on demand (when no space is available to 
+// add / insert a new element).
+
+namespace StackImplementation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Stack
+    {
+        private const int DefaultStackSize = 10;
+        private int currentElementPos = -1;
+        private int[] stackArray;
+
+        public Stack()
+            : this(DefaultStackSize)
+        {
+        }
+
+        public Stack(int initialSize)
+        {
+            this.stackArray = new int[initialSize];
+        }
+
+        public void Push(int elementToAdd)
+        {
+            if (this.currentElementPos == this.stackArray.Length - 1)
+            {
+                this.ResizeStackArray();
+            }
+
+            this.currentElementPos++;
+            this.stackArray[currentElementPos] = elementToAdd;
+        }
+
+        public int Pop()
+        {
+            int elementToRemove = this.stackArray[currentElementPos];
+            this.currentElementPos--;
+            return elementToRemove;
+        }
+
+        public int Size()
+        {
+            return this.currentElementPos + 1;
+        }
+
+        private void ResizeStackArray()
+        {
+            int currentStackSize = stackArray.Length;
+            int[] arrayCopyHolder = new int[currentStackSize];
+            Array.Copy(this.stackArray, arrayCopyHolder, stackArray.Length);
+            this.stackArray = new int[currentStackSize * 2];
+            Array.Copy(arrayCopyHolder, this.stackArray, currentStackSize);
+        }
+    }
+}
