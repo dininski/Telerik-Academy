f5629e61a390ab53886117de0d410815706ccbc2
Vasil Dininski <dininski@gmail.com>
2013-06-02 14:04:30 +0200

    Added Algo academy solutions

---
+++ Algo Academy/2. Data Structures/Data Structures Algo Academy/1. Sorting/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sorting
+{
+    class Program
+    {
+        public static long divider;
+
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] input2 = input.Split(' ');
+            int totalNums = int.Parse(input2[0]);
+            divider = long.Parse(input2[1]);
+            long[] numbersToSort = new long[totalNums];
+            string numbers = Console.ReadLine();
+            string[] number2 = numbers.Split(' ');
+
+            for (int i = 0; i < totalNums; i++)
+            {
+                numbersToSort[i] = long.Parse(number2[i]);
+            }
+
+            Array.Sort(numbersToSort, (x, y) => Comparison(x, y));
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < totalNums - 1; i++)
+            {
+                sb.AppendFormat("{0} ", numbersToSort[i]);
+            }
+
+            sb.AppendFormat("{0} ", numbersToSort[totalNums - 1]);
+            Console.WriteLine(sb.ToString());
+        }
+
+        public static int Comparison(long x, long y)
+        {
+            int result = (x % divider).CompareTo(y % divider);
+            if (result == 0)
+            {
+                return x.CompareTo(y);
+            }
+            else
+            {
+                return result;
+            }
+        }
+    }
+}
---
+++ Algo Academy/2. Data Structures/Data Structures Algo Academy/3. Pipes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pipes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPipes = int.Parse(Console.ReadLine());
+            int pipesNeeded = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
