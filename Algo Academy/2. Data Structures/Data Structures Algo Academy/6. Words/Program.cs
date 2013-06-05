79144d187aabb0b9a55bb78aa228543bf0cd4929
dininski <dininski@gmail.com>
2013-06-05 18:12:22 +0300

    Algo academy problems

---
+++ Algo Academy/2. Data Structures/Data Structures Algo Academy/6. Words/Program.cs
@@ -0,0 +1,125 @@
+﻿namespace _6.Words
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        public static Dictionary<string, int> wordsThatMatch;
+        public static HashSet<string> wordsInSentence;
+        public static HashSet<string> allWords;
+
+        public static void Main(string[] args)
+        {
+            allWords = new HashSet<string>();
+            wordsThatMatch = new Dictionary<string, int>();
+            wordsInSentence = new HashSet<string>();
+            int numberOfLines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                string sentence = Console.ReadLine();
+                
+                string[] words = ParseWords(sentence);
+
+                foreach (var word in words)
+                {
+                    if (!wordsInSentence.Contains(word))
+                    {
+                        wordsInSentence.Add(word);
+                    }
+                }
+            }
+
+            int wordsToFindCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < wordsToFindCount; i++)
+            {
+                string word = Console.ReadLine();
+                if (!allWords.Contains(word))
+                {
+                    allWords.Add(word);
+                }
+
+                if (!wordsThatMatch.ContainsKey(word))
+                {
+                    wordsThatMatch.Add(word, 0);
+                }
+            }
+
+            foreach (var word in wordsInSentence)
+            {
+                foreach (var searchedWord in allWords)
+                {
+                    var lettersInWord = GetLetters(word);
+                    var searchedLetters = GetLetters(searchedWord);
+                    searchedLetters.IntersectWith(lettersInWord);
+
+                    if (searchedLetters.Count == searchedWord.Length)
+                    {
+                        wordsThatMatch[searchedWord]++;
+                    }
+                }
+            }
+
+            PrintDict(wordsThatMatch);
+        }
+
+        public static void PrintHashSet(HashSet<string> words)
+        {
+            foreach (var word in words)
+            {
+                Console.Write("{0}, ", word);
+            }
+        }
+
+        public static void PrintDict(Dictionary<string, int> words)
+        {
+            foreach (var word in words)
+            {
+                Console.WriteLine("{0} -> {1}", word.Key, word.Value);
+            }
+        }
+
+        public static HashSet<char> GetLetters(string word)
+        {
+            var letters = new HashSet<char>();
+            word = word.ToLower();
+            foreach (var letter in word)
+            {
+                if (!letters.Contains(letter))
+                {
+                    letters.Add(letter);
+                }
+            }
+
+            return letters;
+        }
+
+        public static string[] ParseWords(string sentence)
+        {
+            List<string> words = new List<string>();
+            StringBuilder word = new StringBuilder();
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if ((sentence[i] >= 'a' && sentence[i] <= 'z') || (sentence[i] >= 'A' && sentence[i] <= 'Z'))
+                {
+                    word.Append(sentence[i]);       
+                }
+                else
+                {
+                    if (word.ToString() != "")
+                    {
+                        words.Add(word.ToString());
+                    }
+
+                    word = new StringBuilder();
+                }
+            }
+
+            return words.ToArray();
+        }
+    }
+}
---
+++ Algo Academy/2. Data Structures/Data Structures Algo Academy/6. Words/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("6. Words")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("6. Words")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cab404c2-7936-4e82-b33d-6322f1f8c5cd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
