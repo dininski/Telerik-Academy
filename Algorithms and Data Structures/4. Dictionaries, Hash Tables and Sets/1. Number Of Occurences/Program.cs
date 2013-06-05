310b17f1d81c54aaf34498ed5ea294ba02a0fd3c
dininski <dininski@gmail.com>
2013-06-06 01:19:20 +0300

    Solved first two problems

---
+++ Algorithms and Data Structures/4. Dictionaries, Hash Tables and Sets/1. Number Of Occurences/Program.cs
@@ -0,0 +1,42 @@
+﻿// 1. Write a program that counts in a given array of double 
+// values the number of occurrences of each value. Use
+// Dictionary<TKey,TValue>.
+// Example: array = {3, 4, 4, -2.5, 3, 3, 4, 3, -2.5}
+// -2.5 -> 2 times
+// 3 -> 4 times
+// 4 -> 3 times
+
+
+namespace Occurences
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double[] values = { 3, 4, 4, -2.5, 3, 3, 4, 3, -2.5 };
+            PrintOccurences(values);
+        }
+
+        public static void PrintOccurences(double[] values)
+        {
+            Dictionary<double, int> occurences = new Dictionary<double, int>();
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (!occurences.ContainsKey(values[i]))
+                {
+                    occurences.Add(values[i], 0);
+                }
+                occurences[values[i]]++;
+            }
+
+            foreach (var occurence in occurences)
+            {
+                Console.WriteLine("{0} -> {1}", occurence.Key, occurence.Value);
+            }
+        }
+    }
+}
---
+++ Algorithms and Data Structures/4. Dictionaries, Hash Tables and Sets/2. Odd words/Program.cs
@@ -0,0 +1,41 @@
+﻿// 2. Write a program that extracts from a given sequence of
+// strings all elements that present in it odd number of
+// times. Example:
+// {C#, SQL, PHP, PHP, SQL, SQL } -> {C#, SQL}
+namespace OddWords
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] elements = { "C#", "SQL", "PHP", "PHP", "SQL", "SQL" };
+            PrintOddOccurences(elements);
+        }
+
+        public static void PrintOddOccurences(string[] elements)
+        {
+            Dictionary<string, int> occurences = new Dictionary<string, int>();
+
+            foreach (var element in elements)
+            {
+                if (!occurences.ContainsKey(element))
+                {
+                    occurences.Add(element, 0);
+                }
+
+                occurences[element]++;
+            }
+
+            var oddOccurences = occurences.Where(x => x.Value % 2 == 1);
+
+            foreach (var occurence in oddOccurences)
+            {
+                Console.WriteLine("{0} -> {1} times", occurence.Key, occurence.Value);
+            }
+        }
+    }
+}
