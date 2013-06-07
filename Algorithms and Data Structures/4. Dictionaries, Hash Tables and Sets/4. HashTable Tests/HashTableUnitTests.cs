b80fb9e9ca275fdbf54c7b457ec6e36d0aa2115a
dininski <dininski@gmail.com>
2013-06-07 17:30:07 +0300

    Created Hashtable and added unit tests

---
+++ Algorithms and Data Structures/4. Dictionaries, Hash Tables and Sets/4. HashTable Tests/HashTableUnitTests.cs
@@ -0,0 +1,185 @@
+﻿namespace HashTableTests
+{
+    using System;
+    using GenericHashTable;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class HashTableUnitTests
+    {
+        [TestMethod]
+        public void TestEmptyHashTableCount()
+        {
+            HashTable<int, int> integerTable = new HashTable<int, int>();
+            int actualCount = integerTable.Count;
+            Assert.AreEqual(0, actualCount, "The table count is incorrect when table is empty!");
+        }
+
+        [TestMethod]
+        public void TestHashTableCount()
+        {
+            HashTable<int, int> integerTable = new HashTable<int, int>();
+            int numberOfElements = 10;
+
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                integerTable.Add(i, i);
+            }
+
+            int actualCount = integerTable.Count;
+            Assert.AreEqual(numberOfElements, actualCount, "The table count is incorrectly calculated!");
+        }
+
+
+        [TestMethod]
+        public void TestHashTableResizing()
+        {
+            HashTable<int, int> integerTable = new HashTable<int, int>();
+            int numberOfElements = 25;
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                integerTable.Add(i, i * 2);
+            }
+
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                Assert.AreEqual(i * 2, integerTable[i], "Incorrectly resizing the array!");
+            }
+
+            int actualCount = integerTable.Count;
+            Assert.AreEqual(numberOfElements, actualCount, "The table count is incorrectly calculated!");
+        }
+
+        [TestMethod]
+        public void TestHashTableClear()
+        {
+            HashTable<int, int> integerTable = new HashTable<int, int>();
+            int numberOfElements = 10;
+
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                integerTable.Add(i, i);
+            }
+
+            integerTable.Clear();
+
+            int actualCount = integerTable.Count;
+            Assert.AreEqual(0, actualCount, "The table count is incorrect when table is cleared!");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "The element was not correctly removed!")]
+        public void TestHashTableRemove()
+        {
+            HashTable<int, int> integerTable = new HashTable<int, int>();
+            int numberOfElements = 10;
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                integerTable.Add(i, i);
+            }
+
+            integerTable.Remove(3);
+
+            int actualCount = integerTable.Count;
+            Assert.AreEqual(numberOfElements - 1, actualCount, "The table count is incorrect when table is an element is removed!");
+            var shouldFail = integerTable[3];
+        }
+
+        [TestMethod]
+        public void TestHashTableIndexerGetValidKey()
+        {
+            HashTable<int, int> integerTable = new HashTable<int, int>();
+            int numberOfElements = 10;
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                integerTable.Add(i, i + 5);
+            }
+
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                Assert.AreEqual(integerTable[i], i + 5, "The indexer is returning an incorrect value!");
+            }
+        }
+
+        [TestMethod]
+        public void TestHashTableIndexerSetValue()
+        {
+            HashTable<int, int> integerTable = new HashTable<int, int>();
+
+            int numberOfElements = 10;
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                integerTable.Add(i, i + 5);
+            }
+
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                integerTable[i] = i + 2;
+            }
+
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                Assert.AreEqual(i + 2, integerTable[i], "The set indexer is not working correctly");
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "No exception thrown with invalid key!")]
+        public void TestHashTableIndexedGetInvalidKey()
+        {
+            HashTable<int, int> integerTable = new HashTable<int, int>();
+            int numberOfElements = 10;
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                integerTable.Add(i, i + 5);
+            }
+
+            var shouldFail = integerTable[numberOfElements];
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "The hashtable cannot contain duplicate values")]
+        public void TestHashTableAddDuplicate()
+        {
+            HashTable<int, int> integerTable = new HashTable<int, int>();
+            int key = 1;
+            int value = 2;
+            integerTable.Add(key, value);
+            integerTable.Add(key, value);
+        }
+
+        [TestMethod]
+        public void TestHashTableGetKeys()
+        {
+            HashTable<int, int> integerTable = new HashTable<int, int>();
+            int numberOfElements = 10;
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                integerTable.Add(i, i + 10);
+            }
+
+            var keys = integerTable.Keys();
+
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Assert.AreEqual(i, keys[i], "Returning incorrect keys!");
+            }
+        }
+
+        [TestMethod]
+        public void TestHashTableFind()
+        {
+            HashTable<int, int> integerTable = new HashTable<int, int>();
+            int numberOfElements = 10;
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                integerTable.Add(i, i + 10);
+            }
+
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                Assert.AreEqual(i + 10, integerTable.Find(i), "Returning incorrect keys!");
+            }
+        }
+    }
+}
---
+++ Algorithms and Data Structures/4. Dictionaries, Hash Tables and Sets/4. HashTable/HashTable.cs
@@ -0,0 +1,144 @@
+﻿namespace GenericHashTable
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class HashTable<K, V>
+    {
+        private LinkedList<KeyValuePair<K, V>>[] buckets;
+        private int size;
+        private const double MaxLoad = 0.75;
+
+        public HashTable()
+        {
+            this.size = 16;
+            this.buckets = new LinkedList<KeyValuePair<K, V>>[size];
+        }
+
+        public int Count { get; private set; }
+
+        public V this[K key]
+        {
+            get
+            {
+                int bucketHash = key.GetHashCode() % size;
+
+                if (this.buckets[bucketHash] == null)
+                {
+                    throw new ArgumentException("No element found with the provided key.");
+                }
+
+                foreach (var bucketElement in this.buckets[bucketHash])
+                {
+                    if (bucketElement.Key.Equals(key))
+                    {
+                        return bucketElement.Value;
+                    }
+                }
+
+                throw new ArgumentException("No element found with the provided key.");
+            }
+
+            set
+            {
+                int bucketHash = key.GetHashCode() % size;
+                KeyValuePair<K, V> pairToChange = this.buckets[bucketHash].Where(x => x.Key.Equals(key)).First();
+                this.buckets[bucketHash].Remove(pairToChange);
+                KeyValuePair<K, V> updatedPair = new KeyValuePair<K, V>(key, value);
+                this.buckets[bucketHash].AddLast(updatedPair);
+            }
+        }
+
+        public void Add(K key, V value)
+        {
+            this.Count++;
+
+            if (this.Count / this.size >= MaxLoad)
+            {
+                this.Resize();
+            }
+
+            KeyValuePair<K, V> newElement = new KeyValuePair<K, V>(key, value);
+            int bucketHash = key.GetHashCode() % size;
+
+            if (this.buckets[bucketHash] == null)
+            {
+                this.buckets[bucketHash] = new LinkedList<KeyValuePair<K, V>>();
+            }
+
+            if (this.buckets[bucketHash].Select(x => x.Key.Equals(key)).Count() != 0)
+            {
+                throw new ArgumentException("A key with that value already exists in the hash table.");
+            }
+
+            this.buckets[bucketHash].AddLast(newElement);
+        }
+
+        public V Find(K key)
+        {
+            return this[key];
+        }
+
+        public void Remove(K key)
+        {
+            this.Count--;
+            int bucketHash = key.GetHashCode() % size;
+
+            KeyValuePair<K, V> keyValuePairToRemove = this.buckets[bucketHash].Where(x => x.Key.Equals(key)).First();
+
+            this.buckets[bucketHash].Remove(keyValuePairToRemove);
+        }
+
+        public void Clear()
+        {
+            this.buckets = new LinkedList<KeyValuePair<K, V>>[this.size];
+            this.Count = 0;
+        }
+
+        public K[] Keys()
+        {
+            var allKeys = new List<K>();
+
+            foreach (var bucket in this.buckets)
+            {
+                if (bucket != null)
+                {
+                    foreach (var kvPair in bucket)
+                    {
+                        allKeys.Add(kvPair.Key);
+                    }    
+                }
+            }
+
+            return allKeys.ToArray();
+        }
+
+        private void Resize()
+        {
+            size *= 2;
+
+            var newBuckets = new LinkedList<KeyValuePair<K, V>>[size];
+
+            foreach (var bucket in this.buckets)
+            {
+                if (bucket != null)
+                {
+                    foreach (var element in bucket)
+                    {
+                        var bucketHash = element.Key.GetHashCode() % size;
+
+                        if (newBuckets[bucketHash] == null)
+                        {
+                            newBuckets[bucketHash] = new LinkedList<KeyValuePair<K, V>>();
+                        }
+
+                        newBuckets[bucketHash].AddLast(element);
+                    }
+                }
+            }
+
+            this.buckets = newBuckets;
+        }
+    }
+}
