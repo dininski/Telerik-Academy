8c3daa2d7c4d36ca11ef33dafa91ca6b8d86bd01
dininski <dininski@gmail.com>
2013-06-07 18:14:35 +0300

    Started HashedSet implementation

---
+++ Algorithms and Data Structures/4. Dictionaries, Hash Tables and Sets/5. HashedSet/HashedSet.cs
@@ -0,0 +1,56 @@
+﻿namespace GenericHashedSet
+{
+    using GenericHashTable;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class HashedSet<T>
+    {
+        public HashedSet()
+        {
+            this.hashTable = new HashTable<T, int>();
+        }
+
+        private HashTable<T, int> hashTable;
+
+        public int Count
+        {
+            get
+            {
+                return this.hashTable.Count;
+            }
+        }
+
+        public void Add(T element)
+        {
+            this.hashTable.Add(element, 0);
+        }
+
+        public void Find(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T element)
+        {
+            this.hashTable.Remove(element);
+        }
+
+        public void Clear()
+        {
+            this.hashTable.Clear();
+        }
+
+        public HashedSet<T> Union(HashedSet<T> setToUnion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HashedSet<T> Intersect(HashedSet<T> setToUnion) {
+            throw new NotImplementedException();
+        }
+    }
+}
