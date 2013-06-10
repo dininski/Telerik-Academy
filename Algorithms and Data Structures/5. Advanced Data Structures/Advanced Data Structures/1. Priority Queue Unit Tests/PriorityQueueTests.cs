49ce2d39611fd231d5a6ba09a938af34d9680d59
dininski <dininski@gmail.com>
2013-06-10 19:19:11 +0300

    finished priority queue

---
+++ Algorithms and Data Structures/5. Advanced Data Structures/Advanced Data Structures/1. Priority Queue Unit Tests/PriorityQueueTests.cs
@@ -0,0 +1,64 @@
+﻿namespace PQtests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using PQ;
+
+    [TestClass]
+    public class PriorityQueueTests
+    {
+        [TestMethod]
+        public void TestPriorityQueueCount()
+        {
+            PriorityQueue<int> pq = new PriorityQueue<int>();
+            pq.Add(5);
+            pq.Add(6);
+            pq.Add(3);
+            pq.Add(7);
+            pq.Add(11);
+            pq.Add(-5);
+            pq.GetTop();
+            pq.GetTop();
+            Assert.AreEqual(4, pq.Count, "Incorrectly counting the queue elements");
+        }
+
+        [TestMethod]
+        public void TestPriorityQueueOrder_1()
+        {
+            PriorityQueue<int> pq = new PriorityQueue<int>();
+            pq.Add(5);
+            pq.Add(6);
+            pq.Add(3);
+            pq.Add(7);
+            pq.Add(11);
+            pq.Add(-5);
+
+            Assert.AreEqual(-5, pq.GetTop());
+            Assert.AreEqual(3, pq.GetTop());
+            Assert.AreEqual(5, pq.GetTop());
+            Assert.AreEqual(6, pq.GetTop());
+            Assert.AreEqual(7, pq.GetTop());
+            Assert.AreEqual(11, pq.GetTop());
+        }
+
+        [TestMethod]
+        public void TestPriorityQueueOrder_2()
+        {
+            PriorityQueue<int> pq = new PriorityQueue<int>();
+            pq.Add(5);
+            pq.Add(6);
+            pq.Add(3);
+            pq.Add(7);
+            Assert.AreEqual(3, pq.GetTop(), "Incorrect element returned!");
+            pq.Add(11);
+            pq.Add(-5);
+            Assert.AreEqual(-5, pq.GetTop(), "Incorrect element returned!");
+            pq.Add(6);
+            Assert.AreEqual(5, pq.GetTop(), "Incorrect element returned!");
+            Assert.AreEqual(6, pq.GetTop(), "Incorrect element returned!");
+            Assert.AreEqual(6, pq.GetTop(), "Incorrect element returned!");
+            Assert.AreEqual(7, pq.GetTop(), "Incorrect element returned!");
+            Assert.AreEqual(11, pq.GetTop(), "Incorrect element returned!");
+        }
+    }
+}
---
+++ Algorithms and Data Structures/5. Advanced Data Structures/Advanced Data Structures/1. Priority Queue/PriorityQueue.cs
@@ -0,0 +1,95 @@
+﻿namespace PQ
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class PriorityQueue<T> where T : IComparable<T>
+    {
+        public PriorityQueue()
+        {
+            elements = new List<T>();
+            nextElementPosition = 0;
+        }
+
+        private readonly List<T> elements;
+
+        private int nextElementPosition;
+
+        public int Count
+        {
+            get
+            {
+                return this.nextElementPosition;
+            }
+        }
+
+        public void Add(T element)
+        {
+            if (nextElementPosition < this.elements.Count)
+            {
+                this.elements[nextElementPosition] = element;
+            }
+            else
+            {
+                this.elements.Add(element);
+            }
+
+            this.nextElementPosition++;
+            this.Float(this.Count - 1);
+        }
+
+        public T GetTop()
+        {
+            var result = elements[0];
+            this.SwapElements(0, this.Count - 1);
+            this.elements.RemoveAt(this.Count - 1);
+            this.nextElementPosition--;
+            this.Sink(0);
+            return result;
+        }
+
+        private void Float(int element)
+        {
+            int currentElementPos = element;
+            int parent = (currentElementPos - 1) / 2;
+            while (currentElementPos > 0 &&
+                this.elements[parent].CompareTo(this.elements[currentElementPos]) > 0)
+            {
+                this.SwapElements(parent, currentElementPos);
+                currentElementPos = parent;
+                parent = (currentElementPos - 1) / 2;
+            }
+        }
+
+        private void Sink(int pos)
+        {
+            int elementPos = pos;
+
+            while (2 * elementPos < this.Count - 1)
+            {
+                int childPos = 2 * elementPos + 1;
+
+                if (childPos < this.Count - 1 && this.elements[childPos].CompareTo(this.elements[childPos + 1]) > 0)
+                {
+                    childPos++;
+                }
+
+                if (this.elements[elementPos].CompareTo(this.elements[childPos]) < 0)
+                {
+                    break;
+                }
+
+                SwapElements(elementPos, childPos);
+                elementPos = childPos;
+            }
+        }
+
+        private void SwapElements(int parent, int child)
+        {
+            T holder = elements[parent];
+            elements[parent] = elements[child];
+            elements[child] = holder;
+        }
+    }
+}
