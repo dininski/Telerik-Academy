1383ff5e42bdc8f2c41cf8952546376b58a808f9
Vasil Dininski <dininski@gmail.com>
2013-06-21 14:49:42 +0300

    Added combinatorics homework

---
+++ Algorithms and Data Structures/9. Combinatorics/2. Color rabbits/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _2.Color_rabbits
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> rabbits = new Dictionary<int, int>();
+
+            int inputCount = int.Parse(Console.ReadLine());
+            int rabbitsCount = 0;
+
+
+            for (int i = 0; i < inputCount; i++)
+            {
+                int current = int.Parse(Console.ReadLine());
+                if (!rabbits.ContainsKey(current))
+                {
+                    rabbits.Add(current, 0);
+                }
+
+                if (rabbits[current] != current + 1)
+                {
+                    rabbits[current]++;
+                }
+                else
+                {
+                    rabbits[current] = 1;
+                    rabbitsCount += current + 1;
+                }
+            }
+
+            foreach (var item in rabbits)
+            {
+                if (item.Value > 0)
+                {
+                    rabbitsCount += item.Key + 1;                    
+                }
+            }
+
+            Console.WriteLine(rabbitsCount);
+        }
+    }
+}
---
+++ Algorithms and Data Structures/9. Combinatorics/4. Color balls combinations/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace ColorBallsCombinations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Numerics;
+
+    public class Program
+    {
+        static Dictionary<char, int> occurences;
+        static BigInteger comb;
+        static Dictionary<int, BigInteger> factorials;
+
+        public static void Main(string[] args)
+        {
+            comb = 0;
+            string ballsInput = Console.ReadLine();
+            occurences = new Dictionary<char, int>();
+            factorials = new Dictionary<int, BigInteger>();
+
+            factorials.Add(1, 1);
+            factorials.Add(2, 2);
+
+            foreach (var ball in ballsInput)
+            {
+                if (!occurences.ContainsKey(ball))
+                {
+                    occurences.Add(ball, 0);
+                }
+
+                occurences[ball]++;
+            }
+
+            comb = PossibleCombinations(ballsInput.Length, occurences);
+            Console.WriteLine(comb);
+        }
+
+        public static BigInteger PossibleCombinations(int total, Dictionary<char, int> repetitions)
+        {
+            BigInteger denominator = 1;
+
+            foreach (var occurence in occurences)
+            {
+                denominator *= Factorial(occurence.Value);
+            }
+
+            return Factorial(total) / denominator;
+        }
+
+        public static BigInteger Factorial(int number)
+        {
+            if (!factorials.ContainsKey(number))
+            {
+                factorials.Add(number, number * Factorial(number - 1));
+            }
+
+            return factorials[number];
+        }
+    }
+}
