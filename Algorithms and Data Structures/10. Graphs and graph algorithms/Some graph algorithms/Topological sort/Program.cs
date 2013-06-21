3c891fafb94efeb592cbb33eb8cc360d028d5244
Vasil Dininski <dininski@gmail.com>
2013-06-21 12:11:51 +0300

    added exam problems

---
+++ Algorithms and Data Structures/10. Graphs and graph algorithms/Some graph algorithms/Topological sort/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace TopologicalSort
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        static Dictionary<int, List<Node>> children;
+        static Dictionary<int, Node> nodes;
+
+        static void Main(string[] args)
+        {
+            int numberOfConnections = int.Parse(Console.ReadLine());
+            children = new Dictionary<int, List<Node>>();
+            nodes = new Dictionary<int, Node>();
+            for (int i = 0; i < numberOfConnections; i++)
+            {
+                string[] vertexAsString = Console.ReadLine().Split(' ');
+                int parent = int.Parse(vertexAsString[0]);
+                int child = int.Parse(vertexAsString[1]);
+
+                if (!children.ContainsKey(parent))
+                {
+                    children.Add(parent, new List<Node>());
+                }
+
+                if (!children.ContainsKey(child))
+                {
+                    children.Add(child, new List<Node>());
+                }
+
+                if (!nodes.ContainsKey(child))
+                {
+                    Node childNode = new Node(child);
+                    nodes.Add(child, childNode);
+                }
+
+                if (!nodes.ContainsKey(parent))
+                {
+                    Node parentNode = new Node(parent);
+                    nodes.Add(parent, parentNode);
+                }
+
+                nodes[child].NumberOfParents++;
+
+                children[parent].Add(nodes[child]);
+            }
+
+            List<Node> noParents = new List<Node>();
+
+            foreach (var node in nodes)
+            {
+                if (node.Value.NumberOfParents == 0)
+                {
+                    noParents.Add(node.Value);
+                }
+            }
+
+            while (noParents.Count != 0)
+            {
+                var parent = noParents[0];
+
+                foreach (var child in children[parent.Val])
+                {
+                    child.NumberOfParents--;
+                    if (child.NumberOfParents == 0)
+                    {
+                        noParents.Add(child);
+                    }
+                }
+                Console.WriteLine(parent.Val);
+                noParents.Remove(parent);
+            }
+
+        }
+
+        private class Node
+        {
+            public Node(int val)
+            {
+                this.Val = val;
+                this.NumberOfParents = 0;
+            }
+
+            public int NumberOfParents { get; set; }
+            public int Val { get; set; }
+        }
+    }
+}
---
+++ Algorithms and Data Structures/10. Graphs and graph algorithms/Some graph algorithms/Topological sort/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Topological sort")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Topological sort")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("966b824d-3c35-4c64-85b1-e6fcdb7a8f62")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
