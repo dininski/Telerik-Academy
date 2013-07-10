d5c5aba1239597abb31dbcf10ab3ac5cfb4a52ac
Vasil Dininski <dininski@gmail.com>
2013-07-10 22:22:15 +0300

    Added statements

---
+++ Databases/2. SQL Statements/Statements.sql
@@ -0,0 +1,52 @@
+-- Problem 4 - Write a SQL query to find all information about all departments (use "TelerikAcademy" database).
+SELECT * FROM Departments
+
+-- Problem 5 - Write a SQL query to find all department names.
+SELECT Name FROM Departments
+
+-- Problem 6 - Write a SQL query to find the salary of each employee.
+SELECT Salary FROM Employees
+
+-- Problem 7 - Write a SQL to find the full name of each employee.
+SELECT (FirstName + ' ' + LastName) as Name FROM Employees
+
+-- Problem 8 - Write a SQL query to find the email addresses of each employee (by his first and last name).
+-- Consider that the mail domain is telerik.com. Emails should look like “John.Doe@telerik.com". 
+-- The produced column should be named "Full Email Addresses".
+SELECT (FirstName + '.' + LastName + '@telerik.com') AS [Full Email Addresses] FROM Employees
+
+-- Problem 9 - Write a SQL query to find all different employee salaries.
+SELECT DISTINCT Salary FROM Employees
+
+-- Problem 10 - Write a SQL query to find all information about the employees whose job title is “Sales Representative“.
+SELECT * FROM Employees WHERE JobTitle = 'Sales Representative'
+
+-- Problem 11 - Write a SQL query to find the names of all employees whose first name starts with "SA".
+SELECT FirstName FROM Employees WHERE FirstName LIKE 'sa%'
+
+-- Problem 12 - Write a SQL query to find the names of all employees whose last name contains "ei".
+SELECT LastName FROM Employees WHERE LastName LIKE '%ei%'
+
+-- Problem 13 - Write a SQL query to find the salary of all employees whose salary is in the range [20000…30000].
+SELECT Salary FROM Employees WHERE Salary BETWEEN 20000 AND 30000
+
+-- Problem 14 - Write a SQL query to find the names of all employees whose salary is 25000, 14000, 12500 or 23600.
+SELECT FirstName, LastName FROM Employees
+ WHERE Salary = 25000 OR Salary = 14000 OR Salary = 12500 OR Salary = 23600
+
+-- Problem 15 - Write a SQL query to find all employees that do not have manager.
+SELECT * FROM Employees WHERE ManagerID IS NULL
+
+-- Problem 16 - Write a SQL query to find all employees that have salary more than 50000. Order them in decreasing order by salary.
+SELECT * FROM Employees WHERE Salary > 50000 ORDER BY Salary DESC
+
+-- Problem 17 - Write a SQL query to find the top 5 best paid employees.
+SELECT TOP 5 * FROM Employees ORDER BY Salary DESC
+
+-- Problem 18 - Write a SQL query to find all employees along with their address. Use inner join with ON clause.
+SELECT e.FirstName, e.LastName, a.AddressText FROM Employees e
+	INNER JOIN Addresses a ON e.AddressID = a.AddressID
+
+-- Problem 19 - Write a SQL query to find all employees and their address. Use equijoins (conditions in the WHERE clause).
+SELECT e.FirstName, e.LastName, a.AddressText FROM Employees e, Addresses a
+	WHERE e.AddressID = a.AddressID
