bc1edda677fa115d69d948101ee138cdde05bf56
Vasil Dininski <dininski@gmail.com>
2013-07-11 15:08:13 +0300

    Finished Simple sql statements

--- Databases/2. SQL Statements/Statements.sql
+++ Databases/2. SQL Statements/Statements.sql
@@ -1,3 +1,5 @@
+USE TelerikAcademy
+
 -- Problem 4 - Write a SQL query to find all information about all departments (use "TelerikAcademy" database).
 SELECT * FROM Departments
 
@@ -50,3 +52,33 @@ SELECT e.FirstName, e.LastName, a.AddressText FROM Employees e
 -- Problem 19 - Write a SQL query to find all employees and their address. Use equijoins (conditions in the WHERE clause).
 SELECT e.FirstName, e.LastName, a.AddressText FROM Employees e, Addresses a
 	WHERE e.AddressID = a.AddressID
+
+-- Problem 20 - Write a SQL query to find all employees along with their manager.
+SELECT e.FirstName, e.LastName, (m.FirstName + ' ' + m.LastName) AS Manager FROM Employees e
+	INNER JOIN Employees m ON e.ManagerID = m.EmployeeID
+
+-- Problem 21 - Write a SQL query to find all employees, along with their manager and their address.
+-- Join the 3 tables: Employees e, Employees m and Addresses a.
+SELECT e.FirstName, e.LastName, a.AddressText AS Address, (m.FirstName + ' ' + m.LastName) AS Manager
+FROM Employees e
+	INNER JOIN Employees m ON e.ManagerID = m.EmployeeID
+	INNER JOIN Addresses a ON e.AddressID = a.AddressID
+
+-- Problem 22 - Write a SQL query to find all departments and all town names as a single list. Use UNION.
+SELECT t.Name as [Towns and departments] FROM Towns t
+UNION
+SELECT d.Name FROM Departments d
+
+-- Problem 23 - Write a SQL query to find all the employees and the manager for each of them along with
+-- the employees that do not have manager. Use right outer join. Rewrite the query to use left outer join.
+SELECT e.firstName, e.LastName, (m.FirstName + ' ' + m.LastName) AS Manager
+	FROM Employees m
+	RIGHT JOIN Employees e ON m.EmployeeID = e.ManagerID
+
+-- Problem 24 - Write a SQL query to find the names of all employees from the
+-- departments "Sales" and "Finance" whose hire year is between 1995 and 2005.
+SELECT e.FirstName, e.LastName, d.Name, e.HireDate
+	FROM Employees e
+	RIGHT JOIN Departments d ON e.DepartmentID = d.DepartmentID
+	WHERE (d.Name = 'Sales' OR d.Name = 'Finance') 
+		AND e.HireDate BETWEEN '1995' AND '2005'
