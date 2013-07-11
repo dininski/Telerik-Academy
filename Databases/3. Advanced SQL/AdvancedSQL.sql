e27d98ed6932f8894184d9869085c212aaa3b3bb
Vasil Dininski <dininski@gmail.com>
2013-07-11 15:54:19 +0300

    solved first 11 problems

--- Databases/3. Advanced SQL/AdvancedSQL.sql
+++ Databases/3. Advanced SQL/AdvancedSQL.sql
@@ -1,20 +1,61 @@
 USE TelerikAcademy
 
--- Problem 1 - Write a SQL query to find the names and salaries of the employees that take the minimal salary in the company. Use a nested SELECT statement.
+-- Problem 1 - Write a SQL query to find the names and salaries of the employees
+-- that take the minimal salary in the company. Use a nested SELECT statement.
 SELECT e.FirstName, e.LastName, e.Salary
 	FROM Employees e WHERE e.Salary = 
 		(SELECT MIN(Salary) From Employees)
 
--- Problem 2 - Write a SQL query to find the names and salaries of the employees that have a salary that is up to 10% higher than the minimal salary for the company.
--- Problem Write a SQL query to find the full name, salary and department of the employees that take the minimal salary in their department. Use a nested SELECT statement.
--- Problem Write a SQL query to find the average salary in the department #1.
--- Problem Write a SQL query to find the average salary  in the "Sales" department.
--- Problem Write a SQL query to find the number of employees in the "Sales" department.
--- Problem Write a SQL query to find the number of all employees that have manager.
--- Problem Write a SQL query to find the number of all employees that have no manager.
--- Problem Write a SQL query to find all departments and the average salary for each of them.
--- Problem Write a SQL query to find the count of all employees in each department and for each town.
--- Problem Write a SQL query to find all managers that have exactly 5 employees. Display their first name and last name.
+-- Problem 2 - Write a SQL query to find the names and salaries of the employees
+-- that have a salary that is up to 10% higher than the minimal salary for the company.
+SELECT e.FirstName, e.LastName, e.Salary FROM Employees e
+	WHERE e.Salary < (SELECT MIN(Salary) From Employees) * 1.1
+
+-- Problem 3 - Write a SQL query to find the full name, salary and department of the
+-- employees that take the minimal salary in their department. Use a nested SELECT statement.
+SELECT FirstName, LastName, DepartmentID, Salary
+FROM Employees e
+WHERE Salary = 
+  (SELECT MIN(Salary) FROM Employees 
+   WHERE DepartmentID = e.DepartmentID)
+ORDER BY DepartmentID
+
+-- Problem 4 - Write a SQL query to find the average salary in the department #1.
+SELECT AVG(e.Salary) AS [Average Salary] FROM Employees e
+	WHERE e.DepartmentID = 1
+
+-- Problem 5 - Write a SQL query to find the average salary in the "Sales" department.
+SELECT AVG(e.Salary) AS [Average Salary] FROM Employees e
+	JOIN Departments d ON e.DepartmentID = d.DepartmentID
+	WHERE d.Name = 'Sales'
+
+-- Problem 6 - Write a SQL query to find the number of employees in the "Sales" department.
+SELECT COUNT(*) AS [Number of people in Sales] FROM Employees e
+	JOIN Departments d ON e.DepartmentID = d.DepartmentID
+	WHERE d.Name = 'Sales'
+
+-- Problem 7 - Write a SQL query to find the number of all employees that have manager.
+SELECT COUNT(ManagerID) AS [Employees with managers] FROM Employees e
+
+-- Problem 8 - Write a SQL query to find the number of all employees that have no manager.
+SELECT COUNT(*) AS [Employess without managers] FROM Employees e
+	WHERE e.ManagerID IS NULL
+
+-- Problem 9 - Write a SQL query to find all departments and the average salary for each of them.
+SELECT d.Name AS Department, AVG(e.Salary) FROM Employees e
+	JOIN Departments d ON e.DepartmentID = d.DepartmentID
+	GROUP BY d.Name
+
+-- Problem 10 - Write a SQL query to find the count of all employees in each department and for each town.
+SELECT d.Name AS Department, t.Name AS Town, COUNT(*) FROM Employees e
+	JOIN Departments d ON e.DepartmentID = d.DepartmentID
+	JOIN Addresses a ON e.AddressID = a.AddressID
+	JOIN Towns t ON a.TownID = t.TownID
+	GROUP BY t.Name, d.Name
+
+-- Problem 11 - Write a SQL query to find all managers that have exactly 5 employees. Display their first name and last name.
+
+
 -- Problem Write a SQL query to find all employees along with their managers. For employees that do not have manager display the value "(no manager)".
 -- Problem Write a SQL query to find the names of all employees whose last name is exactly 5 characters long. Use the built-in LEN(str) function.
 -- Problem Write a SQL query to display the current date and time in the following format "day.month.year hour:minutes:seconds:milliseconds". Search in  Google to find how to format dates in SQL Server.
