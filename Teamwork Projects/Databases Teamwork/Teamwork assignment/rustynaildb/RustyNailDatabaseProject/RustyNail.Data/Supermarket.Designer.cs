e6b877c47933a36341fe9dfa3d9d6c902c1e8ced
Vasil Dininski <dininski@gmail.com>
2013-07-22 12:16:08 +0300

    Added entity

---
+++ Teamwork Projects/Databases Teamwork/Teamwork assignment/rustynaildb/RustyNailDatabaseProject/RustyNail.Data/Supermarket.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'D:\projects\telerik\_git\Telerik-Academy\Teamwork Projects\Databases Teamwork\Teamwork assignment\rustynaildb\RustyNailDatabaseProject\RustyNail.Data\Supermarket.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
