ce997c47cf0b1b8172432b834d57904cd07bc193
Vasil Dininski <dininski@gmail.com>
2013-10-02 18:59:38 -0700

    Revert "Revert "updated app, removed bugs, added error message""
    This reverts commit adc956ed97ec9f38dcbc8be399feae7937e6db71.

--- Windows 8 Apps with WinJS/RottenBrowser/js/default.js
+++ Windows 8 Apps with WinJS/RottenBrowser/js/default.js
@@ -12,8 +12,7 @@
     app.addEventListener("activated", function (args) {
         if (args.detail.kind === activation.ActivationKind.launch) {
             if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
-                // TODO: This application has been newly launched. Initialize
-                // your application here.
+
             } else {
                 // TODO: This application has been reactivated from suspension.
                 // Restore application state here.
